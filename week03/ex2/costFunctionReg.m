f156d0c27f40f249de063c2ed45669cad3e28576
Tomas Mussi <tomasmussi@gmail.com>
2020-03-04 22:32:54 -0300

    Adding regularized solution to logistic regression

--- week03/ex2/costFunctionReg.m
+++ week03/ex2/costFunctionReg.m
@@ -18,7 +18,30 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+% First, cost function:
+pos = find(y==1); neg = find(y == 0);
+sig = sigmoid(X * theta);
+posValues = -1 .* log(sig(pos,:));
+negValues = -1 .* log(1 .- (sig(neg,:)));
+J = (sum(posValues) + sum(negValues)) / m;
 
+% Add regularization to cost function J(theta)
+% Do not regularize theta[0] 
+reg = [theta(2:rows(theta)) .^2]';
+%printf("Reg %.2f \n",reg);
+%printf("[%d] %.2f \n",lambda, ((lambda / (2*m)) * sum(reg)));
+J = J + ((lambda / (2*m)) * sum(reg));
+
+% Second, derivative of J(theta)
+temp1 = sigmoid(X * theta) - y;
+temp2 = temp1 .* X;
+temp3 = temp2' * ones(m, 1);
+grad = temp3 ./ m;
+% Add regularization
+%printf("theta %.2f \n",theta);
+derReg = (lambda/m) .* [0, theta(2:rows(theta))']';
+%printf("[%d] %.2f \n",lambda, derReg);
+grad = grad + derReg;
 
 
 
