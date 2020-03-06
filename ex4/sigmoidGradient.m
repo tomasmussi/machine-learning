0108bab5fe9cde5987604f8cfb10608235bd7f9b
Tomas Mussi <tomasmussi@gmail.com>
2020-03-06 13:37:03 -0300

    Adding sigmoid derived function + random initialization of Theta matrix

--- ex4/sigmoidGradient.m
+++ ex4/sigmoidGradient.m
@@ -12,7 +12,8 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
+calc = sigmoid(z);
+g = calc .* (1 .- calc);
 
 
 
