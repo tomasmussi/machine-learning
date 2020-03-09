27bce18229c961979aa1f07d89b3b3d11d7f7475
Tomas Mussi <tomasmussi@gmail.com>
2020-03-08 21:44:29 -0300

    Finished exercise 6

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,7 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
+    for idx = 1:length(vocabList)
+      w_check = vocabList{idx};
+      if (strcmp(w_check, str) == 1)
+        word_indices = [word_indices ; idx];
+      endif
+    endfor
 
 
 
